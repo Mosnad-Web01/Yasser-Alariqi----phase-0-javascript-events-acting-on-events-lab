c02e722eb8a16eae253cebf73750cf18331a76f7
Yasser Esam <yaaser10esam1010@gmail.com>
2024-08-18 15:07:50 +0300

    Ready test code

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+function moveDodgerLeft() {
+    dodger.style.left = "0px";
+}
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left < 360) { 
+        dodger.style.left = `${left + 10}px`;
+    }
+}
